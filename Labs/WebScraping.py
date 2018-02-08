eb86e587a1db5faf6def0c10f4a535c51f1c458c
Unknown <valdialva@hotmail.com>
2018-02-08 08:48:47 -0500

    Update Files

--- Labs/WebScraping.py
+++ Labs/WebScraping.py
@@ -1,3 +1,7 @@
+#Alvaro Valdivieso
+#Sebastian Ramos
+
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,29 +10,35 @@ req = requests.get('http://elmercioco.com/')
 #Parse HTML
 soup = BeautifulSoup(req.text, "html.parser")
 
-#Get 
+#Get news from headlights
 news  = soup.find_all('ul', ('class','article-block-big'))
 
+#Get the links from the news selected
 links = []
 
 for link in news.find_all('a'):
     links.append(link.get('href'))
 
+#Get the actual article links
 articles = []
 
 for article in links:
     articles.append(requests.get(article))
     
+#Get the text from the news    
 texts = []
 for text in articles:
     texts.append(BeautifulSoup(text.text, "html.parser"))
 
+#Get the titles
 titles = []
 for title in texts:
     titles.append(title.h1)
     
-bodys = []
+#Get the body of the news    
+bodies = []
 for body in texts:
-    bodys.append(body.find_all('div', ('class','block-content')))
+    bodies.append(body.find_all('div', ('class','block-content')))
     
-#
+print {titles}
+print (bodies)
